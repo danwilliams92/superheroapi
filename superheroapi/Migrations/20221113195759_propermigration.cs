6c009985366c98462a9adf9c44a392c822e34eea
danwilliams92 <dan-ver.2007@hotmail.co.uk>
2022-11-15 21:13:13 +0000

    Add project files.

---
+++ superheroapi/Migrations/20221113195759_propermigration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace superheroapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class propermigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
